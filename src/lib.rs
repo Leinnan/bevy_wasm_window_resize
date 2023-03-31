f93b7b50b3046eee9efba1ab354606856f425c05
Piotr Siuszko <siuszko@zoho.com>
2023-03-31 16:25:55 +0200

    Init

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use bevy::prelude::{App, Plugin};
+
+pub struct WindowResizePlugin;
+
+impl Plugin for WindowResizePlugin {
+    #[cfg(target_arch = "wasm32")]
+    fn build(&self, app: &mut App) {
+        app.add_system(handle_browser_resize);
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    fn build(&self, _app: &mut App) {}
+}
+
+#[cfg(target_arch = "wasm32")]
+fn handle_browser_resize(
+    mut primary_query: bevy::ecs::system::Query<
+        &mut bevy::window::Window,
+        bevy::ecs::query::With<bevy::window::PrimaryWindow>,
+    >,
+) {
+    for mut window in &mut primary_query {
+        let wasm_window = web_sys::window().unwrap();
+        let (target_width, target_height) = (
+            wasm_window.inner_width().unwrap().as_f64().unwrap() as f32,
+            wasm_window.inner_height().unwrap().as_f64().unwrap() as f32,
+        );
+        if window.resolution.width() != target_width || window.resolution.height() != target_height
+        {
+            window.resolution.set(target_width, target_height);
+        }
+    }
+}
